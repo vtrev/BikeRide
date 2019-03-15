f2735e9aae51656af631c995fbf8c929658bda69
vtrev <baloyi.trev@gmail.com>
2019-03-15 11:56:42 +0200

    fixed issue #2 and moved logic

--- src/bicycles/rides/FunRide.java
+++ src/bicycles/rides/FunRide.java
@@ -3,41 +3,35 @@ package bicycles.rides;
 import bicycles.Bicycle;
 import bicycles.BicycleType;
 import java.util.ArrayList;
-import java.util.HashMap;
 
 public class FunRide {
 
     private ArrayList<Bicycle> bikeList= new ArrayList<Bicycle>();
-    HashMap<String,Integer> bikeTypeMap = new HashMap<String,Integer>();
-    private int currentBikeCount = 0;
     private int maxBikeIntake = 0;
 
     public FunRide(int maxBikeIn){
         this.maxBikeIntake = maxBikeIn;
     }
     public String accept(Bicycle bikeIn){
-        boolean canAdd = ((currentBikeCount+1) <= maxBikeIntake);
+
+        boolean canAdd = ((bikeList.size()+1) <= maxBikeIntake);
         if(canAdd){
             bikeList.add(bikeIn);
-            currentBikeCount++;
-            String bikeTypeString = bikeIn.getBicycleType().toString();
-            if(bikeTypeMap.containsKey(bikeTypeString)){
-                int currentBikeCountForType = bikeTypeMap.get(bikeTypeString);
-                bikeTypeMap.put(bikeTypeString,currentBikeCountForType+1);
-            }else {
-                bikeTypeMap.put(bikeTypeString,1);
-            }
             return "Bike Accepted";
         }
         return "Bike Rejected";
     }
 
     public int getCountForType(BicycleType bikeTypeIn){
-        return bikeTypeMap.get(bikeTypeIn.toString());
+        int countForTypeHold = 0;
+        for(Bicycle bike : bikeList){
+            if (bike.getBicycleType() == bikeTypeIn){
+                countForTypeHold++;
+            }
+        }
+        return countForTypeHold;
     }
-
     public int getEnteredCount(){
-        return this.currentBikeCount;
+        return this.bikeList.size();
     }
-
 }
